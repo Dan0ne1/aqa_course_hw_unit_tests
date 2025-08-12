e53715f8ce2d653ab4973e0befc3e57a8c14f04c
dan <danu_91@mail.ru>
2025-08-12 18:52:11 +0300

    HW-6

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
   У вас есть массив названий пицц вашего конкурента.
   Создайте скрипт с циклом, который будет проверять ваш набор названий пицц (массив) 
@@ -13,7 +15,41 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
-let resultNull;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
+function checkUniquePizzas(myPizzas) {
+  let resultUnique = [];
+  let resultNull = null;
+
+  // Приводим все названия к нижнему регистру для корректного сравнения
+  const competitorPizzasLower = [];
+  for (const pizza of competitorPizzas) {
+    competitorPizzasLower.push(pizza.toLowerCase());
+  }
+
+  for (const pizza of myPizzas) {
+    if (!competitorPizzasLower.includes(pizza.toLowerCase())) {
+      resultUnique.push(pizza);
+    }
+  }
+
+  // Если resultUnique пуст, значит все пиццы есть у конкурента
+  if (resultUnique.length === 0) {
+    resultNull = null;
+    resultUnique = null;
+  }
+
+  return { resultUnique, resultNull };
+}
+
+// Проверяем для myPizzasT1
+const resultT1 = checkUniquePizzas(myPizzasT1);
+console.log('Для myPizzasT1:', resultT1);
+
+// Проверяем для myPizzasT2
+const resultT2 = checkUniquePizzas(myPizzasT2);
+console.log('Для myPizzasT2:', resultT2);
 
 export { resultNull, resultUnique };
